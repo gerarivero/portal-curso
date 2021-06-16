09440cb98645acc7414389cff370484f65fae594
sebastian <sebastian.yaccuzzi@gmail.com>
2021-06-16 19:28:52 -0300

    dashboard

--- hotspot/hotspot/urls.py
+++ hotspot/hotspot/urls.py
@@ -18,5 +18,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path('portal/', include('portal.urls')),
+    path('dashboard/', include('dashboard.urls')),
     path('admin/', admin.site.urls),
 ]
